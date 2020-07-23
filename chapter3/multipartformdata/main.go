42a26351851601c6b25d0fa5f9e30d9509a9a541
ichi_taro3 <taro3.ichi@gmail.com>
2020-07-23 23:20:10 +0900

    add some scripts

---
+++ chapter3/multipartformdata/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	_ = writer.WriteField("name", "Michael Jackson")
+	fileWriter, err := writer.CreateFormFile("thumbnail", "photo.jpgo")
+	if err != nil {
+		panic(err)
+	}
+	readFile, err := os.Open("photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+	_, _ = io.Copy(fileWriter, readFile)
+	_ = writer.Close()
+
+	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+}
